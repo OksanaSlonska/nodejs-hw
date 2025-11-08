ffbb8e47b016b0c187734618508d8929c534f02a
OksanaSlonska <slonskayaoksana@gmail.com>
2025-11-08 16:02:58 +0200

    fix HW

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -6,5 +6,6 @@ export const connectMongoDB = async () => {
     console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('❌ Failed to connect to MongoDB:', error.message);
+    process.exit(1);
   }
 };
