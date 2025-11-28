2e302edbc5f5cedec5a1e57c2055fe13bb1e078d
OksanaSlonska <slonskayaoksana@gmail.com>
2025-11-28 18:59:23 +0200

    add HW5

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
@@ -22,6 +23,7 @@ app.use(logger);
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
